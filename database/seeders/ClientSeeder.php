137d0882cc96cb025c396f8dd9618c03e18a76c7
Sacedon <christiansacedon3@gmail.com>
2023-07-13 21:24:51 +0800

    email-send

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -13,6 +13,33 @@ class ClientSeeder extends Seeder
      */
     public function run(): void
     {
-        Client::factory(30)->create();
+        Client::factory(20)->create();
+        $cl = [
+            [
+                'last_name' => 'Sacedon',
+                'first_name' => 'Christian James',
+                'middle_name' => 'Rulona',
+                'address' => 'Purok 3. Tinangnan, Tubigon, Bohol',
+                'birth_date' => '2001-10-18',
+                'phone_number' => '09486306766',
+                'email' => 'christiansacedon3@gmail.com',
+                'initial_deposit' => 10000
+            ],
+            [
+                'last_name' => 'Jack',
+                'first_name' => 'Ripper',
+                'middle_name' => 'Mercha',
+                'address' => ' Clover, Japan',
+                'birth_date' => '1967-11-03',
+                'phone_number' => '09307665033',
+                'email' => 'christiansacedon5@gmail.com',
+                'initial_deposit' => 10000
+            ]
+
+        ];
+
+        foreach($cl as $c) {
+            Client::create($c);
+        }
     }
 }
